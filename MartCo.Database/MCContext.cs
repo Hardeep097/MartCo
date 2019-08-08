469fb0687af9396ef6026fb9e8ad79d88a840dc4
Hardeep Singh <harrypabla99@gmail.com>
2019-08-07 23:49:25 -0400

    Before database connection Azure

---
+++ MartCo.Database/MCContext.cs
@@ -0,0 +1,16 @@
+﻿using MartCo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartCo.Database
+{
+    public class MCContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
